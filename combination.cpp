dec19b6fd9e7049844759483cb067b1db188df73
25cd060 <25cd060@rmkec.ac.in>
2026-06-05 21:05:34 +0530

    Update combination.cpp

---
+++ combination.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+string str;
+cin >> str;
+len = str.length();
+for (int i = 1; i < (1 << len); i++) {
+string combination;
+for (int shift = 0; shift < len; shift++)
+if (i & (1 << shift))
+combination += str[shift];
+cout << combination << endl;}
+return 0;
+}
